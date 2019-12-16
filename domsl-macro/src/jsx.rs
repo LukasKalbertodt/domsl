767291c7e46c1d3d988cf063e2344399c2fcb68c
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-12-16 21:36:03 +0100

    Minor refactoring

--- domsl-macro/src/jsx.rs
+++ domsl-macro/src/jsx.rs
@@ -1,4 +1,4 @@
-use proc_macro2::{Ident, Span, TokenStream, TokenTree};
+use proc_macro2::{Ident, Spacing, Span, TokenStream, TokenTree};
 use quote::{quote, quote_spanned};
 use snax::{SnaxAttribute, SnaxItem, SnaxTag, SnaxSelfClosingTag};
 
@@ -7,6 +7,39 @@ use crate::{
     html::TagInfo,
 };
 
+
+
+pub(crate) fn parse_outer(input: TokenStream) -> Result<(Ident, TokenStream), Error> {
+    let mut iter = input.into_iter();
+
+    let document = match iter.next() {
+        Some(TokenTree::Ident(ident)) => ident,
+        Some(tt) => return Err(Error::expected("ident", tt)),
+        None => return Err(Error::eof()),
+    };
+
+    match (iter.next(), iter.next()) {
+        (Some(TokenTree::Punct(p0)), Some(TokenTree::Punct(p1)))
+            if p0.as_char() == '='
+                && p1.as_char() == '>'
+                && p0.spacing() == Spacing::Joint
+                && p1.spacing() == Spacing::Alone => {}
+        (Some(tt), _) => return Err(Error::expected("`=>`", tt)),
+        (None, _) => return Err(Error::eof()),
+    }
+
+    let body = match iter.next() {
+        Some(TokenTree::Group(g)) => g.stream(),
+        Some(tt) => return Err(Error::expected("`{}` block", tt)),
+        None => return Err(Error::eof()),
+    };
+
+    if let Some(tt) = iter.next() {
+        return Err(Error::new(tt.span(), "expected end of input, but found this"));
+    }
+
+    Ok((document, body))
+}
 
 /// The main entry point to generate the output code.
 pub(crate) fn gen(root: &SnaxItem, document: &Ident) -> Result<TokenStream, Error> {
--- domsl-macro/src/lib.rs
+++ domsl-macro/src/lib.rs
@@ -1,57 +1,29 @@
 extern crate proc_macro;
 
-use proc_macro2::{Ident, Spacing, TokenStream, TokenTree};
+use proc_macro::{TokenStream as TokenStream1};
+use proc_macro2::TokenStream;
 
 use crate::error::Error;
 
 mod error;
-mod gen;
+mod jsx;
 mod html;
 
 
 
 #[proc_macro]
-pub fn jsx(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn jsx(input: TokenStream1) -> TokenStream1 {
+    fn run(input: TokenStream) -> Result<TokenStream, Error> {
+        let (document, body) = jsx::parse_outer(input)?;
+        let item = snax::parse(body.into())?;
+
+        jsx::gen(&item, &document)
+    }
+
     run(input.into())
         .unwrap_or_else(|e| e.error_tokens)
         .into()
 }
 
-fn run(input: TokenStream) -> Result<TokenStream, Error> {
-    let (document, body) = parse_outer(input)?;
-    let item = snax::parse(body.into())?;
 
-    gen::gen(&item, &document)
-}
 
-fn parse_outer(input: TokenStream) -> Result<(Ident, TokenStream), Error> {
-    let mut iter = input.into_iter();
-
-    let document = match iter.next() {
-        Some(TokenTree::Ident(ident)) => ident,
-        Some(tt) => return Err(Error::expected("ident", tt)),
-        None => return Err(Error::eof()),
-    };
-
-    match (iter.next(), iter.next()) {
-        (Some(TokenTree::Punct(p0)), Some(TokenTree::Punct(p1)))
-            if p0.as_char() == '='
-                && p1.as_char() == '>'
-                && p0.spacing() == Spacing::Joint
-                && p1.spacing() == Spacing::Alone => {}
-        (Some(tt), _) => return Err(Error::expected("`=>`", tt)),
-        (None, _) => return Err(Error::eof()),
-    }
-
-    let body = match iter.next() {
-        Some(TokenTree::Group(g)) => g.stream(),
-        Some(tt) => return Err(Error::expected("`{}` block", tt)),
-        None => return Err(Error::eof()),
-    };
-
-    if let Some(tt) = iter.next() {
-        return Err(Error::new(tt.span(), "expected end of input, but found this"));
-    }
-
-    Ok((document, body))
-}
